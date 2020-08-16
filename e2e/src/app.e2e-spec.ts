6d5a17b269479671f28b47c180fe0a1f7fd54b28
Himanshu kumar <dorado2041@gmail.com>
2020-08-16 10:09:03 +0530

    Project name changed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-advance app is running!');
+    expect(page.getTitleText()).toEqual('echarts-angular-project app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-advance'`, () => {
+  it(`should have as title 'echarts-angular-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-advance');
+    expect(app.title).toEqual('echarts-angular-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-advance app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('echarts-angular-project app is running!');
   });
 });
